f281e328a81ca0337aca149cfaf045f5149fe93b
behzodkhoja <nigmatxojayevbehzodxoja@gmail.com>
2023-05-07 15:59:14 +0500

    Added updates to the IRepository interface

--- MoneyManagment.DAL/IRepository/IRepository.cs
+++ MoneyManagment.DAL/IRepository/IRepository.cs
@@ -1,13 +1,21 @@
-﻿using System;
+﻿using MoneyManagment.Domain.Commons;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MoneyManagment.DAL.IRepository
 {
-    public class IRepository
+    public interface IRepository<TEntity> where TEntity : Auditable
     {
-
+        ValueTask<TEntity> InsertAsync(TEntity entity);
+        ValueTask<TEntity> UpdateAsync(TEntity entity);
+        ValueTask<bool> DeleteAsync(Expression<Func<TEntity, bool>> pridacate);
+        ValueTask<TEntity> SelectAsync(Expression<Func<TEntity, bool>> pridacate);
+        IQueryable<TEntity> SelectAllAsync(Expression<Func<TEntity, bool>> pridacate = null);
+        ValueTask SaveAsync();
     }
 }
+
