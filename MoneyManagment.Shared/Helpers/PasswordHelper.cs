f8a1847c6e44ee6c68fead0c3fff2081073fbf50
behzodkhoja <nigmatxojayevbehzodxoja@gmail.com>
2023-05-09 11:03:57 +0500

    Created HttpCotextHelper and PasswordHelper classes

--- MoneyManagment.Shared/Helpers/PasswordHelper.cs
+++ MoneyManagment.Shared/Helpers/PasswordHelper.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace MoneyManagment.Shared.Helpers
 {
-    internal class PasswordHelper
+    public class PasswordHelper
     {
+        public static string Hash(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+    }
+    public static bool Verify(string password, string passwordHash)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, passwordHash);
     }
 }
