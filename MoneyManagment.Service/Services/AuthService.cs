f3c39b358c928c8efbc7d61d489f07e1980ddebd
behzodkhoja <nigmatxojayevbehzodxoja@gmail.com>
2023-05-09 11:54:22 +0500

    Added AuthService

--- MoneyManagment.Service/Services/AuthService.cs
+++ MoneyManagment.Service/Services/AuthService.cs
@@ -1,10 +1,15 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using MoneyManagment.Domain.Entities;
 using MoneyManagment.Service.DTOs.Users;
+using MoneyManagment.Service.Exceptions;
 using MoneyManagment.Service.Interfaces;
 using MoneyManagment.Shared.Helpers;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -24,7 +29,35 @@ namespace MoneyManagment.Service.Services
         public async Task<LoginResultDto> AuthenticateAsync(string email, string password)
         {
             var user = await this.userService.RetrieveByEmailAsync(email);
-            if(user == null || !PasswordHelper.Verify(password, user.Password))
+            if (user == null || !PasswordHelper.Verify(password, user.Password))
+                throw new MoneyManagmentException(400, "Email or password in correct");
+            return new LoginResultDto
+            {
+                Token = GenerateToken(user)
+            };
+        }
+
+        private string GenerateToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenKey = Encoding.UTF8.GetBytes(configuration["JWT:Key"]);
+            var tokenDescripter = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim("Id", user.Id.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role.ToString()),
+                    new Claim(ClaimTypes.Name, user.FirstName)
+                }),
+                Audience = configuration["JWT:Audience"],
+                Issuer = configuration["JWT:Issuer"],
+                IssuedAt= DateTime.UtcNow,
+                Expires = DateTime.UtcNow.AddMinutes(double.Parse(configuration["JWT:Expires"])),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(tokenKey), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescripter);
+            return tokenHandler.WriteToken(token);
         }
     }
 }
